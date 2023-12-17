ace699fcf3d30bc6a25303668f939071c586d624
Min Thant Nyi <minthantnyi1212@gmail.com>
2023-12-17 14:42:01 +0630

    basic test architecture and first test

---
+++ auth/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+
+import {
+  currentUserRouter,
+  signInRouter,
+  signOutRouter,
+  signUpRouter,
+} from './routes'
+import { errorHandler } from './middlewares'
+import { NotFoundError } from './errors'
+
+const app = express()
+
+app.set('trust proxy', true)
+app.use(json())
+app.use(cookieSession({ signed: false, secure: true }))
+
+app.use(signUpRouter)
+app.use(signInRouter)
+app.use(signOutRouter)
+app.use(currentUserRouter)
+
+app.all('*', () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,8 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
-import cookieSession from 'cookie-session'
-
-import {
-  currentUserRouter,
-  signInRouter,
-  signOutRouter,
-  signUpRouter,
-} from './routes'
-import { errorHandler } from './middlewares'
-import { NotFoundError } from './errors'
+import { app } from './app'
 import { connect } from './loaders'
 
-const app = express()
 const port = process.env.PORT || 3000
 
-app.set('trust proxy', true)
-app.use(json())
-app.use(cookieSession({ signed: false, secure: true }))
-
-app.use(signUpRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(currentUserRouter)
-
-app.all('*', () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
 ;(async () => {
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY is missing')
 
